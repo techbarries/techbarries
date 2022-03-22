3a68ce48529872cc2d223954eb15e6141e2c1eb8
samaddestring <samad@destring.com>
2022-03-22 22:37:11 +0500

    user create api added

---
+++ authentication/models.py
@@ -0,0 +1,97 @@
+from django.db import models
+
+from helpers.models import TrackingModel
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser, UserManager, PermissionsMixin)
+
+
+class MyUserManager(UserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+        
+class User(AbstractBaseUser, PermissionsMixin,TrackingModel):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username and password are required. Other fields are optional.
+    """
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        ("username"),
+        max_length=150,
+        unique=False,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique":("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(("first name"), max_length=150, blank=True)
+    last_name = models.CharField(("last name"), max_length=150, blank=True)
+    profile_picture_url = models.CharField(("profile picture url"), max_length=250, blank=True)
+    phone_number = models.CharField(("phone number"), max_length=50, blank=True)
+    country = models.CharField(("Country"), max_length=100, blank=True)
+    email = models.EmailField(("email address"), blank=False,unique=True)
+    is_staff = models.BooleanField(
+        ("staff status"),
+        default=False,
+        help_text=("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        ("active"),
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = MyUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    @property
+    def token(self):
+        return ""
---
+++ authentication/serializers.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+import imp
+from unittest.util import _MAX_LENGTH
+from rest_framework import serializers
+from authentication.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=('id','first_name','last_name','profile_picture_url','is_active','phone_number','username','email')
+    def create(self,validated_data):
+        return User.objects.create_user(**validated_data)
+            
