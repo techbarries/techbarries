7d70b88d87a81dcb09ab19074a383ba791ecc758
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-04-05 14:22:31 +0500

    pulseuser api modification

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     path('pulse-user',RegisterAPIView.as_view(),name="pulseUser"),
     path('users',UserListAPIView.as_view(),name="users"),
     path('user/<int:user_id>/devices',DeviceByUserView.as_view({'get': 'list',}),name="device-by-user"),
-    path('user/token/<slug:user_token>',UserByTokenView.as_view({'get': 'retrieve',}),name="user-by-token")
+    path('user/token/<slug:user_token>',UserByTokenView.as_view(),name="user-by-token")
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,16 @@
+from ast import Not
+from email import message
 import imp
 from telnetlib import STATUS
 from urllib import response
-from django.shortcuts import render
 from rest_framework.generics import GenericAPIView,CreateAPIView,ListAPIView,RetrieveUpdateDestroyAPIView
 from authentication.models import Device, User
 from authentication.serializers import DeviceSerializer, PulseUserSerializer, UserSerializer
 from rest_framework import response,status
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -15,12 +18,14 @@ class RegisterAPIView(GenericAPIView):
     serializer_class=PulseUserSerializer
     def post(self,request):
         serializer=self.serializer_class(data=request.data)
-
+        res={"status":True,"message":"User created successfully","data":{}}
         if serializer.is_valid():
             serializer.save()
-            return response.Response(serializer.data,status=status.HTTP_201_CREATED)
-        
-        return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            res.update(data=serializer.data)
+            return Response(res,status=status.HTTP_200_OK)
+        res.update(status=False,message="Validation error",data={"errors":serializer.errors})    
+        return Response(res,status=status.HTTP_200_OK)
+        # return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserListAPIView(ListAPIView):
@@ -54,9 +59,23 @@ class DeviceByUserView(viewsets.ModelViewSet):
     # filter_backends = (DjangoFilterBackend,)
     # search_fields = ['user_id']
 
-class UserByTokenView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    lookup_field = 'user_token'
-    def get_queryset(self):
-        user_token = self.request.parser_context['kwargs'].get('user_token')
-        return User.objects.all().filter(user_token=user_token)
+# class UserByTokenView(viewsets.ModelViewSet):
+#     serializer_class = UserSerializer
+#     lookup_field = 'user_token'
+#     def get_queryset(self):
+#         user_token = self.request.parser_context['kwargs'].get('user_token')
+#         return User.objects.all().filter(user_token=user_token)
+
+class UserByTokenView(APIView):
+    serializer_class=PulseUserSerializer
+    def get(self,request,user_token,format=None):
+        user_token=user_token
+        res={"status":True,"message":"User found","data":{}}
+        if user_token is not None:
+            user=User.objects.filter(user_token=user_token).first()
+            if user is not None:
+                serializer=UserSerializer(user)
+                res.update(data=serializer.data)
+                return Response(res)
+        res.update(status=False,message="Not found")
+        return Response(res)
