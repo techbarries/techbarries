c7997b67982fa78d511478075d93975e6440e325
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-05-13 16:21:27 +0500

    university obj in events

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,6 +3,7 @@ import imp
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from authentication.models import Device, SmsOTP, User
+from events.serializers import UniversitySerializer
 
 class PulseUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,6 +13,7 @@ class PulseUserSerializer(serializers.ModelSerializer):
         return User.objects.create_user(**validated_data)
           
 class UserSerializer(serializers.ModelSerializer):
+    university=UniversitySerializer(read_only=True)
     class Meta:
         model=User
         fields=('id','uid','user_token','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','country','university','email')
