4860c21972864e37adb255275a4e84646d71c86f
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-05-20 00:27:56 +0500

    my events count added to all the user apis

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,6 +3,7 @@ import imp
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 from authentication.models import Device, SmsOTP, User
+from events.models import EventStatus
 from events.serializers import UniversitySerializer
 
 class PulseUserSerializer(serializers.ModelSerializer):
@@ -14,10 +15,13 @@ class PulseUserSerializer(serializers.ModelSerializer):
           
 class UserSerializer(serializers.ModelSerializer):
     university=UniversitySerializer(read_only=True)
+    event_count=serializers.SerializerMethodField()
     class Meta:
         model=User
-        fields=('id','uid','user_token','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','country','university','email')
-
+        fields=('id','uid','user_token','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','country','university','email','event_count')
+    def get_event_count(self,obj):
+        eventStatus=EventStatus.objects.filter(user_id=obj.id,hosted=True)
+        return eventStatus.count()
  
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
