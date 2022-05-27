c6984679d610285929681b8519461a2a10672d2e
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-05-27 22:00:01 +0500

    added event count and uni obj to user update

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,11 +7,16 @@ from events.models import EventStatus
 from events.serializers import UniversitySerializer
 
 class PulseUserSerializer(serializers.ModelSerializer):
+    university=UniversitySerializer(read_only=True)
+    event_count=serializers.SerializerMethodField()
     class Meta:
         model=User
-        fields=('id','uid','user_token','username','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','date_of_birth','job_title','degree_title','country','university','email','profile_access_type')
+        fields=('id','uid','user_token','username','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','date_of_birth','job_title','degree_title','country','university','email','event_count','profile_access_type')
     def create(self,validated_data):
         return User.objects.create_user(**validated_data)
+    def get_event_count(self,obj):
+        eventStatus=EventStatus.objects.filter(user_id=obj.id,hosted=True)
+        return eventStatus.count()    
           
 class UserSerializer(serializers.ModelSerializer):
     university=UniversitySerializer(read_only=True)
