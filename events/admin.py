a8f05b3f5f73c795d22431e2f3f71d0f4509499b
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-05-29 19:50:13 +0500

    added image preview

--- events/admin.py
+++ events/admin.py
@@ -1,16 +1,34 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from events.models import Age, Dress, Event, EventImage, Food, Music, University, Venue, VenueImage
 
 # Register your models here.
 class VenueImageInline(admin.TabularInline):
     model = VenueImage
+    readonly_fields = ('image_preview',)
     extra = 3
+    def image_preview(self, obj):
+        # ex. the name of column is "image"
+        if obj.image:
+            return mark_safe('<img src="{0}" width="50" style="object-fit:contain" />'.format(obj.image.url))
+        else:
+            return '(No image)'
+
+    image_preview.short_description = 'Preview' 
 class VenueAdmin(admin.ModelAdmin):
     inlines = [ VenueImageInline, ]
 class EventImageInline(admin.TabularInline):
     model = EventImage
+    readonly_fields = ('image_preview',)
     extra = 3
+    def image_preview(self, obj):
+        # ex. the name of column is "image"
+        if obj.image:
+            return mark_safe('<img src="{0}" width="50" style="object-fit:contain" />'.format(obj.image.url))
+        else:
+            return '(No image)'
+
+    image_preview.short_description = 'Preview'    
 class EventAdmin(admin.ModelAdmin):
     inlines = [ EventImageInline, ]
 admin.site.register(University)
