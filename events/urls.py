2515c966a439dc0266aaed91b082499e2495cf9d
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-06-27 19:00:49 +0500

    event details api added

--- events/urls.py
+++ events/urls.py
@@ -1,4 +1,4 @@
-from events.views import CreateEventAPIView, EventImageDeleteAPIView,EventNotificaionStatusAPIView,EventShareAPIView, EventListAPIView,EventNearMeListAPIView, EventStatusAPIView, PastEventListAPIView, UpcomingEventListAPIView 
+from events.views import CreateEventAPIView, EventImageDeleteAPIView,EventNotificaionStatusAPIView,EventShareAPIView, EventListAPIView,EventNearMeListAPIView, EventStatusAPIView, PastEventListAPIView, UpcomingEventListAPIView,EventDetailAPIView 
 from django.urls import path
 
 urlpatterns=[
@@ -6,6 +6,7 @@ urlpatterns=[
     path('image/delete/<int:id>',EventImageDeleteAPIView.as_view(),name="event-image-delete"),
     path('share',EventShareAPIView.as_view(),name="event-status-update"),
     path('user/<int:user_id>/notificaion/<int:notificaion_id>/event/<int:event_id>/<slug:status>',EventNotificaionStatusAPIView.as_view(),name="event-notificaion-accept-update"),
+    path('<int:event_id>/user/<int:user_id>',EventDetailAPIView.as_view(),name="event-detail"),
     path('<int:event_id>/user/<int:user_id>/<slug:status>',EventStatusAPIView.as_view(),name="event-status-update"),
     path('events/<int:user_id>',EventListAPIView.as_view(),name="events"),
     path('near-me/events/<int:user_id>/<str:latitude>/<str:longitude>',EventNearMeListAPIView.as_view(),name="events"),
--- events/views.py
+++ events/views.py
@@ -103,7 +103,100 @@ class EventImageDeleteAPIView(APIView):
             res={"status":False,"message":"Event Image not found","data":{}}
          return Response(res,status=status.HTTP_200_OK) 
         
+class EventDetailAPIView(APIView):
+    def get(self,request,event_id,user_id):
+        user=User.objects.filter(id=user_id).first()
+        if user is None:
+            res={"status":False,"message":"User not found","data":{}}
+            return Response(res)
+        event=Event.objects.filter(id=event_id).first()
+        if event is None:
+            res={"status":False,"message":"Event not found","data":{}}
+            return Response(res)
+        serializer=EventSerializer(event)
+        event=serializer.data        
+        # guest user's details
+        if len(event['guests'])>0:
+            guestUsers=[];
+            for guest in event['guests']:
+                user=User.objects.filter(id=guest).first()
+                if user is not None:
+                    serializer=UserSerializer(user)
+                    userItem={}
+                    userItem=serializer.data
+                    eventStatus=EventStatus.objects.filter(user_id=user.id).all()
+                    user_score=0
+                    if eventStatus.count() > 0:
+                        for eventStatusItem in eventStatus:
+                            if eventStatusItem.hosted:
+                                user_score+=7
+                            if eventStatusItem.checked_in:
+                                user_score+=6
+                            if eventStatusItem.pinned:
+                                user_score+=5
+                            if eventStatusItem.paid:
+                                user_score+=4
+                            if eventStatusItem.guest_list:
+                                user_score+=3
+                            if eventStatusItem.invited:
+                                user_score+=2
+                            if eventStatusItem.public:
+                                user_score+=1
+                            if eventStatusItem.not_going:
+                                user_score+=0
 
+                    userItem.update({"user_score":user_score})
+                    guestUsers.append(userItem)
+
+                event['guests']=guestUsers
+                lint_score=0
+                eventStatus=EventStatus.objects.filter(event_id=event['id']).all()    
+                if eventStatus.count() > 0:
+                    for eventStatusItem in eventStatus:
+                        if eventStatusItem.checked_in:
+                            lint_score+=6
+                        if eventStatusItem.pinned:
+                            lint_score+=5
+                        if eventStatusItem.paid:
+                            lint_score+=4
+                        if eventStatusItem.guest_list:
+                            lint_score+=3
+                        if eventStatusItem.invited:
+                            lint_score+=2
+                #status list for current user
+                status_list={'checked_in':False,'pinned':False,'liked':False,'joined':False,'going':False,'not_going':False,'paid':False,'guest_list':False,'invited':False}
+                eventStatusByUser=EventStatus.objects.filter(user_id=user_id,event_id=event['id']).first()    
+                if eventStatusByUser is not None:
+                    if eventStatusByUser.checked_in:
+                        status_list['checked_in']=True
+                    if eventStatusByUser.pinned:
+                        status_list['pinned']=True
+                    if eventStatusByUser.liked:
+                        status_list['liked']=True    
+                    if eventStatusByUser.joined:
+                        status_list['joined']=True
+                    if eventStatusByUser.going:
+                        status_list['going']=True
+                    if eventStatusByUser.not_going:
+                        status_list['not_going']=True                        
+                    if eventStatusByUser.paid:
+                        status_list['paid']=True
+                    if eventStatusByUser.guest_list:
+                        status_list['guest_list']=True
+                    if eventStatusByUser.invited:
+                        status_list['invited']=True 
+
+                
+                event['status_list']=status_list
+                event['lint_score']=lint_score
+                isLive=False
+                if datetime.strptime(event['event_end_date'],"%Y-%m-%d")  >= datetime.today() >= datetime.strptime(event['event_start_date'],"%Y-%m-%d"):
+                    current_time = datetime.now().strftime('%H:%M:%S')
+                    if event['event_start_time'] and event['event_end_time'] and is_between(current_time,(event['event_start_time'],event['event_end_time'])):
+                        isLive=True
+                event['is_live']=isLive
+                res={"status":True,"message":"event found","data":{"event":event}}
+                return Response(res)
 class EventStatusAPIView(APIView):
     """Following are possible values for the status types
     \n"checked_in","checked_out","pinned","un_pinned","joined","going","not_going","liked","un_liked",leave"
