4becad5e0b73df32b8d35ce71bc6e5a47fee1e73
abdulsamadtariq <abdulsamadtariq786@gmail.com>
2022-07-26 11:24:33 +0500

    added event user score and venue promoter user score

--- events/serializers.py
+++ events/serializers.py
@@ -17,9 +17,10 @@ class EventUserSerializer(serializers.ModelSerializer):
     university=UserUniversitySerializer(read_only=True)
     event_count=serializers.SerializerMethodField()
     friends_count=serializers.SerializerMethodField()
+    user_score=serializers.SerializerMethodField()
     class Meta:
         model=User
-        fields=('id','uid','user_token','username','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','date_of_birth','job_title','degree_title','country','university','email','event_count','friends_count','profile_access_type','user_type')
+        fields=('id','uid','user_token','username','first_name','last_name','profile_picture_url','profile_picture_image','is_active','phone_number','date_of_birth','job_title','degree_title','country','university','email','event_count','friends_count','user_score','profile_access_type','user_type')
     def get_event_count(self,obj):
         # eventStatus=EventStatus.objects.filter(user_id=obj.id,hosted=True)
         events=Event.objects.filter(user_id=obj.id,archived=False).all()
@@ -27,6 +28,30 @@ class EventUserSerializer(serializers.ModelSerializer):
     def get_friends_count(self,obj):
         friends=Friends.objects.filter(Q(sent_to_user_id=obj.id )|Q(sent_by_user_id=obj.id),status=True).all()
         return friends.count()                
+    def get_user_score(self,obj):
+        eventStatus=EventStatus.objects.filter(user_id=obj.id).all()
+        user_score=0
+        if eventStatus.count() > 0:
+            for eventStatusItem in eventStatus:
+                if eventStatusItem.hosted:
+                    user_score+=7
+                if eventStatusItem.checked_in:
+                    user_score+=6
+                if eventStatusItem.pinned:
+                    user_score+=5
+                if eventStatusItem.paid:
+                    user_score+=4
+                if eventStatusItem.guest_list:
+                    user_score+=3
+                if eventStatusItem.invited:
+                    user_score+=2
+                if eventStatusItem.public:
+                    user_score+=1
+                if eventStatusItem.not_going:
+                    user_score+=0
+
+        return user_score           
+
 
 class UniversitySerializer(serializers.ModelSerializer):
     class Meta:
